16670036169395162fd1fb917305da2de3c3b013
Vi Cuong <tranvicuongit@gmail.com>
2017-07-30 02:05:01 +0700

    Fix dynamic config

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,115 @@
 var app = angular.module('HTMLApp', ['ngRoute', 'ngAnimate']);
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+  //dynamic config from json array
+  $.getJSON("/assets/json/menu.json", function (data) {
+    angular.forEach(data, function (ele, index) {
+      $routeProvider
+        .when(ele.link, {
+          templateUrl: ele.templateUrl,
+          controller: ele.controller
+        });
+    });
+  }).fail(function (error) {
+    console.log(error);
+  });
+
   $routeProvider
-    .when('/', {
-      templateUrl: 'pages/_home.html',
-      controller: 'HomeCtrl'
+
+
+    // for first loading app
+    .when('/loading', {
+      templateUrl: '/pages/_loading.html',
+      controller: 'LoadingCtrl'
     })
-    .when('/danh-sach-san-pham', {
-      templateUrl: 'pages/products/_product.list.html',
-      controller: 'ProductsCtrl'
+
+    // for dynamic config from json
+    .when('/', {
+      redirectTo: '/loading'
+    })
+    .when('/:for', {
+      redirectTo: '/loading'
+    })
+    .otherwise({
+      controller: 'ErrorCtrl'
     });
 
-  //$locationProvider.html5Mode(false, base: false);
   $locationProvider
     .html5Mode(false)
     .hashPrefix('!');
+
 }]);
 
+app.controller('ErrorCtrl', ['$route', '$routeParams', '$location',
+  function ($route, $routeParams, $location) {
+  }]);
+
+app.controller('LoadingCtrl', ['$route', '$routeParams', '$location', '$scope', '$rootScope', '$templateCache', '$window',
+  function ($route, $routeParams, $location, $scope, $rootScope, $templateCache, $window) {
+    $scope.$on('$viewContentLoaded', function () {
+      // remove standard code
+      var path = '/:for';
+      var tmplUrl = $route.routes[path].templateUrl;
+      $templateCache.remove(tmplUrl);
+      delete ($route.routes[path]);
+      delete ($route.routes[path + '/']);
+      $window.location.href = "#!/" + ($routeParams.for == undefined ? "" : $routeParams.for);
+    });
+  }]);
+
 app.controller('HomeCtrl', ['$route', '$routeParams', '$location',
-  function HomeCtrl($route, $routeParams, $location) {
-    this.$route = $route;
-    this.$location = $location;
-    this.$routeParams = $routeParams;
+  function ($route, $routeParams, $location) {
+    console.log("home");
   }]);
 
 app.controller('ProductsCtrl', ['$route', '$routeParams', '$location',
-  function ProductsCtrl($route, $routeParams, $location) {
-    this.$route = $route;
-    this.$location = $location;
-    this.$routeParams = $routeParams;
-
-
+  function ($route, $routeParams, $location) {
+    console.log("list-pro");
   }]);
 
-app.run(['$rootScope', '$location', '$timeout', '$http', 'ToolService',
-  function ($rootScope, $location, $timeout, $http, toolService) {
-    
+app.run(['$rootScope', '$location', '$timeout', '$http', '$route', '$templateCache', '$routeParams',
+  function ($rootScope, $location, $timeout, $http, $route, $templateCache, $routeParams) {
+    // get header
+    $.getJSON("/assets/json/menu.json", function (data) {
+      $rootScope.ListHeader = data;
+    }).fail(function (error) {
+      console.log(error);
+    });
+
+    //$location.path($location.url());
+    $location.path($location.url());
+
+    $rootScope.$on('$routeChangeStart', function (next, current) {
+    });
+    $rootScope.$on('$routeChangeSuccess', function (next, current) {
+    });
+    $rootScope.$on('$routeChangeError', function (next, current) {
+    });
+    $rootScope.$on('$viewContentLoaded', function () {
+      $rootScope.$on('finishedRenderHeaderSelected', function (event) {
+        $("a").each(function () {
+          if ($(this).attr("href").indexOf("#!") < 0) {
+            $(this).attr("href", "#!/" + $(this).attr("href").replace(/\//g, ""));
+          }
+        });
+      });
+    });
   }
 ]);
+
+app.directive('onFinishRender', function ($timeout) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attr) {
+      if (scope.$last === true) {
+        $timeout(function () {
+          scope.$emit(attr.onFinishRender);
+        });
+      }
+    }
+  }
+});
 
 
 angular.bootstrap(document, ['HTMLApp']);
